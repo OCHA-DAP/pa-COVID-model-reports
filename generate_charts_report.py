aa47e7114e8c329056a168a2f608b0e47f509e50
Leonardo Milano <milanoleonardo@yahoo.it>
2020-07-14 17:29:11 +0200

    adding updated graphs

--- generate_charts_report.py
+++ generate_charts_report.py
@@ -74,7 +74,7 @@ def generate_key_figures(country_iso3):
     who_covid=get_who_covid(country_iso3,min_date=LAST_MONTH,max_date=FOUR_WEEKS)
     who_deaths_today=who_covid.loc[TODAY,'CumDeath']
     who_cases_today=who_covid.loc[TODAY,'CumCase']
-    print(f'Current situation {TODAY}: {who_cases_today} cases, {who_deaths_today} deaths')
+    print(f'Current situation {TODAY}: {who_cases_today:.0f} cases, {who_deaths_today:.0f} deaths')
 
     bucky_npi=get_bucky(country_iso3,admin_level='adm0',min_date=TODAY,max_date=THREE_MONTHS,npi_filter='npi')
     reporting_rate=bucky_npi['CASE_REPORT'].mean()*100
@@ -85,10 +85,10 @@ def generate_key_figures(country_iso3):
     max_deaths_npi=bucky_npi[bucky_npi['q']==0.75].loc[FOUR_WEEKS,'cumulative_deaths']
     print(f'- Projection date:{FOUR_WEEKS}')
     
-    print(f'- ESTIMATED CASE REPORTING RATE {reporting_rate}')
-    print(f'-- NPI: ESTIMATED Reff NPI {reff_npi}')
-    print(f'-- NPI: Projected reported cases in 4w: {min_cases_npi} - {max_cases_npi}')
-    print(f'-- NPI: Projected reported deaths in 4w: {min_deaths_npi} - {max_deaths_npi}')
+    print(f'- ESTIMATED CASE REPORTING RATE {reporting_rate:.2f}')
+    print(f'-- NPI: ESTIMATED Reff NPI {reff_npi:.2f}')
+    print(f'-- NPI: Projected reported cases in 4w: {min_cases_npi:.0f} - {max_cases_npi:.0f}')
+    print(f'-- NPI: Projected reported deaths in 4w: {min_deaths_npi:.0f} - {max_deaths_npi:.0f}')
     
     bucky_no_npi=get_bucky(country_iso3,admin_level='adm0',min_date=TODAY,max_date=THREE_MONTHS,npi_filter='no_npi')
     reff_no_npi=bucky_no_npi['Reff'].mean()
@@ -97,9 +97,9 @@ def generate_key_figures(country_iso3):
     min_deaths_no_npi=bucky_no_npi[bucky_no_npi['q']==0.25].loc[FOUR_WEEKS,'cumulative_deaths']
     max_deaths_no_npi=bucky_no_npi[bucky_no_npi['q']==0.75].loc[FOUR_WEEKS,'cumulative_deaths']
     # print(bucky_no_npi.loc[FOUR_WEEKS,:])
-    print(f'--- no_npi: ESTIMATED Reff no_npi {reff_no_npi}')
-    print(f'--- no_npi: Projected reported cases in 4w: {min_cases_no_npi} - {max_cases_no_npi}')
-    print(f'--- no_npi: Projected reported deaths in 4w: {min_deaths_no_npi} - {max_deaths_no_npi}')
+    print(f'--- no_npi: ESTIMATED Reff no_npi {reff_no_npi:.2f}')
+    print(f'--- no_npi: Projected reported cases in 4w: {min_cases_no_npi:.0f} - {max_cases_no_npi:.0f}')
+    print(f'--- no_npi: Projected reported deaths in 4w: {min_deaths_no_npi:.0f} - {max_deaths_no_npi:.0f}')
     
 
 def generate_daily_projections(country_iso3,parameters):
