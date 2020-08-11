af0f095f0520ec17fc8a7d9a7dee998b0c0c8cb1
Leonardo Milano <milanoleonardo@yahoo.it>
2020-08-11 11:04:16 +0200

    updated code, partial refactoring

--- generate_charts_report.py
+++ generate_charts_report.py
@@ -1,19 +1,13 @@
 import utils
-import os
-import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
-
-import matplotlib.dates as mdates
-import requests
+import os
 import geopandas as gpd
 from datetime import datetime,timedelta
-from scipy.optimize import curve_fit
-import numpy as np
 
-from utils import set_matlotlib
+from utils import *
 
-ASSESSMENT_DATE='2020-07-29'
+ASSESSMENT_DATE='2020-07-29' 
 # TODAY = date.today()
 TODAY = datetime.strptime(ASSESSMENT_DATE, '%Y-%m-%d').date()
 FOUR_WEEKS = TODAY + timedelta(days=28)
@@ -28,15 +22,7 @@ MAX_QUANTILE=0.75
 CONFIG_FILE = 'config.yml'
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 WHO_COVID_URL='https://docs.google.com/spreadsheets/d/e/2PACX-1vSe-8lf6l_ShJHvd126J-jGti992SUbNLu-kmJfx1IRkvma_r4DHi0bwEW89opArs8ZkSY5G2-Bc1yT/pub?gid=0&single=true&output=csv'
-WHO_COVID_FILENAME=WHO_COVID_FILENAME='WHO_data/Data_ WHO Coronavirus Covid-19 Cases and Deaths - WHO-COVID-19-global-data.csv'
-
-HLX_TAG_TOTAL_CASES = "#affected+infected+confirmed+total"
-HLX_TAG_TOTAL_DEATHS = "#affected+infected+dead+total"
-HLX_TAG_DATE = "#date"
-HLX_TAG_ADM2_PCODE='#adm2+pcode'
-
-FIG_SIZE=(8,6)
-
+WHO_COVID_FILENAME='WHO_data/Data_ WHO Coronavirus Covid-19 Cases and Deaths - WHO-COVID-19-global-data.csv'
 
 NPI_COLOR='green'
 NO_NPI_COLOR='red'
@@ -48,7 +34,7 @@ def main(country_iso3='AFG',download_covid=False):
     parameters = utils.parse_yaml(CONFIG_FILE)[country_iso3]
     if download_covid:
         # Download latest covid file tiles and read them in
-        get_covid_data(WHO_COVID_URL,f'{DIR_PATH}/{WHO_COVID_FILENAME}')
+        download_who_covid_data(WHO_COVID_URL,f'{DIR_PATH}/{WHO_COVID_FILENAME}')
     set_matlotlib(plt)
     print('\n\n\n')
     print(f'{country_iso3}')
@@ -59,44 +45,6 @@ def main(country_iso3='AFG',download_covid=False):
     create_subnational_map(country_iso3, parameters)
     calculate_subnational_trends(country_iso3, parameters)
     # plt.show()
-
-def download_url(url, save_path, chunk_size=128):
-    r = requests.get(url, stream=True)
-    with open(save_path, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=chunk_size):
-            fd.write(chunk)
-    print(f'Downloaded "{url}" to "{save_path}"')
-
-def get_covid_data(url, save_path):
-    # download covid data from HDX
-    print(f'Getting upadated COVID data from WHO')
-    try:
-        download_url(url, save_path)
-    except Exception:
-        print(f'Cannot download COVID file from from HDX')
-
-def get_bucky(country_iso3,admin_level,min_date,max_date,npi_filter):
-    # get bucky with NPIs 
-    bucky_df=pd.read_csv(f'Bucky_results/{country_iso3}_{npi_filter}/{admin_level}_quantiles.csv')
-    bucky_df['date']=pd.to_datetime(bucky_df['date']).dt.date
-    # if country_iso3=='AFG':
-    #     # scaling afghanistan to take into account delays in reporting
-    #     bucky_df['date']=bucky_df['date']+timedelta(days=10)
-    #     bucky_df['cumulative_deaths']=bucky_df['cumulative_deaths']*1147/807
-    #     bucky_df['cumulative_cases_reported']=bucky_df['cumulative_cases_reported']*35229/32022
-    #     bucky_df['hospitalizations']=bucky_df['hospitalizations']*35229/32022
-    #     bucky_df['cases_per_100k']=bucky_df['cases_per_100k']*35229/32022
-    # if country_iso3=='SDN':
-    #     # scaling afghanistan to take into account delays in reporting
-    #     bucky_df['date']=bucky_df['date']+timedelta(days=11)
-    #     bucky_df['cumulative_deaths']=bucky_df['cumulative_deaths']*668/691
-    #     bucky_df['cumulative_cases_reported']=bucky_df['cumulative_cases_reported']*10527/10084
-    #     bucky_df['hospitalizations']=bucky_df['hospitalizations']*10527/10084
-    #     bucky_df['cases_per_100k']=bucky_df['cases_per_100k']*10527/10084
-    bucky_df=bucky_df[(bucky_df['date']>=min_date) &\
-                        (bucky_df['date']<=max_date)]
-    bucky_df=bucky_df.set_index('date')
-    return bucky_df
 
 def extract_reff(country_iso3):
     bucky_npi=get_bucky(country_iso3,admin_level='adm0',min_date=TODAY,max_date=FOUR_WEEKS,npi_filter='npi')
@@ -110,55 +58,13 @@ def extract_reff(country_iso3):
     print(f'Estimated doubling time No NPI {dt_no_npi}, Reff {r_no_npi}')
     
 
-def get_bucky_dt_reff(df_bucky):
-    # start fit
-    dates_proj = df_bucky.index
-    xfit=[(x-dates_proj[0]).days for x in dates_proj]
-    yfit = df_bucky['cumulative_cases_reported']
-    initial_caseload=yfit[0]
-    initial_parameters=[initial_caseload,0.03]
-    # # TODO check quality of the fit
-    popt, _ = curve_fit(func,xfit,yfit,p0=initial_parameters)
-    doubling_time_fit=np.log(2)/popt[1]
-    # parameters suggested by Matt
-    Tg = 7.
-    Ts = 5.
-    n = 3
-    f = .4
-    m = 2
-    r = np.log(2)/doubling_time_fit
-    Te=calc_Te(Tg, Ts, n, f)
-    reff=calc_Reff(m, n, Tg, Te, r)
-    # old method
-    # # https://www.acpjournals.org/doi/10.7326/M20-0504
-    # infectious_period=5.2
-    # # according to eq 1 in https://www.mdpi.com/2306-7381/7/1/2/htm#B17-vetsci-07-00002
-    # reff=1+(np.log(2)/doubling_time_fit)*infectious_period
-    return doubling_time_fit,reff
-
-def calc_Te(Tg, Ts, n, f):
-    num = 2.0 * n * f / (n + 1.0) * Tg - Ts
-    den = 2.0 * n * f / (n + 1.0) - 1.0
-    return num / den
-
-def calc_Reff(m, n, Tg, Te, r):
-    tdiff = Tg - Te
-    num = 2.0 * n * r / (n + 1.0) * (Tg - Te) * (1.0 + r * Te / m) ** m
-    den = 1.0 - (1.0 + 2.0 * r / (n + 1.0) * (Tg - Te)) ** (-n)
-    return num / den
-
-# not used at the moment
-# def calc_Ti(Te, Tg, n):
-    # return (Tg - Te) * 2.0 * n / (n + 1.0)
-
-def func(x, p0, beta):
-    return p0 * np.exp(x*beta)
 
 def generate_key_figures(country_iso3):
 
-    who_covid=get_who_covid(country_iso3,min_date=LAST_TWO_MONTHS,max_date=FOUR_WEEKS)
+    who_covid=get_who(WHO_COVID_FILENAME,country_iso3,min_date=LAST_TWO_MONTHS,max_date=FOUR_WEEKS)
     who_deaths_today=who_covid.loc[TODAY,'CumDeath']
     who_cases_today=who_covid.loc[TODAY,'CumCase']    
+    CFR=who_deaths_today/who_cases_today*100
     # get weekly new cases
     who_covid.index = pd.to_datetime(who_covid.index)
     new_WHO_w=who_covid.groupby(['ISO_3_CODE']).resample('W').sum()[['NewCase','NewDeath']]
@@ -170,7 +76,6 @@ def generate_key_figures(country_iso3):
     new_WHO_w['NewDeath_PercentChange'] = new_WHO_w.groupby('ISO_3_CODE')['NewDeath'].pct_change()
     trend_w_cases=new_WHO_w.loc[new_WHO_w.index[-1],'NewCase_PercentChange']*100
     trend_w_deaths=new_WHO_w.loc[new_WHO_w.index[-1],'NewDeath_PercentChange']*100
-    CFR=who_deaths_today/who_cases_today*100
     print(f'Current situation {TODAY}: {who_cases_today:.0f} cases, {who_deaths_today:.0f} deaths')
     print(f'CFR {TODAY}: {CFR:.1f}')
     print(f'Weekly new cases wrt last week: {trend_w_cases:.0f}% cases, {trend_w_deaths:.0f}% deaths')
@@ -212,7 +117,6 @@ def generate_model_projections(country_iso3,parameters):
     # generate plot with long term projections of daily cases
     bucky_npi=get_bucky(country_iso3,admin_level='adm0',min_date=TODAY,max_date=FOUR_WEEKS,npi_filter='npi')
     bucky_no_npi=get_bucky(country_iso3,admin_level='adm0',min_date=TODAY,max_date=FOUR_WEEKS,npi_filter='no_npi')
-
     draw_model_projections(country_iso3,bucky_npi,bucky_no_npi,parameters,'hospitalizations')
 
 def draw_model_projections(country_iso3,bucky_npi,bucky_no_npi,parameters,metric):
@@ -264,40 +168,10 @@ def draw_model_projections(country_iso3,bucky_npi,bucky_no_npi,parameters,metric
     print(f'----{metric} NO NPI {FOUR_WEEKS}: {metric_4w_no_npi_min:.0f} - {metric_4w_no_npi_max:.0f}')
     fig.savefig(f'Outputs/{country_iso3}/projection_{metric}.png')
 
-def get_subnational_covid_data(parameters,aggregate,min_date,max_date):
-    # get subnational from COVID parameterization repo
-    subnational_covid=pd.read_csv(parameters['subnational_cases_url'])
-    subnational_covid[HLX_TAG_DATE]=pd.to_datetime(subnational_covid[HLX_TAG_DATE]).dt.date
-    subnational_covid=subnational_covid[(subnational_covid[HLX_TAG_DATE]>=min_date) &\
-                                        (subnational_covid[HLX_TAG_DATE]<=max_date)]
-    if aggregate:
-        # date and adm2 are the unique keys
-        dates=sorted(set(subnational_covid[HLX_TAG_DATE]))
-        adm2pcodes=set(subnational_covid[HLX_TAG_ADM2_PCODE])
-        unique_keys=[HLX_TAG_DATE,HLX_TAG_ADM2_PCODE]
-        # create a multi-index to fill missing combinations with None values
-        mind = pd.MultiIndex.from_product([dates,adm2pcodes],names=unique_keys)
-        subnational_covid=subnational_covid.set_index(unique_keys).reindex(mind,fill_value=None)
-        # forward fill missing values for each pcode
-        subnational_covid=subnational_covid.groupby(HLX_TAG_ADM2_PCODE).ffill()
-        # sum by date
-        subnational_covid=subnational_covid.groupby(HLX_TAG_DATE).sum()
-    return subnational_covid
-
-def get_who_covid(country_iso3,min_date,max_date):
-    # Get national level data from WHO
-    who_covid=pd.read_csv(WHO_COVID_FILENAME)
-    who_covid=who_covid[who_covid['ISO_3_CODE']==country_iso3]
-    who_covid['date_epicrv']=pd.to_datetime(who_covid['date_epicrv']).dt.date
-    who_covid=who_covid[(who_covid['date_epicrv']>=min_date) &\
-                        (who_covid['date_epicrv']<=max_date)]
-    who_covid=who_covid.set_index('date_epicrv')
-    return who_covid
-
 def generate_data_model_comparison(country_iso3,parameters):
     # generate plot with subnational data, WHO data and projections
     subnational_covid=get_subnational_covid_data(parameters,aggregate=True,min_date=LAST_TWO_MONTHS,max_date=FOUR_WEEKS)
-    who_covid=get_who_covid(country_iso3,min_date=LAST_TWO_MONTHS,max_date=FOUR_WEEKS)
+    who_covid=get_who(WHO_COVID_FILENAME,country_iso3,min_date=LAST_TWO_MONTHS,max_date=FOUR_WEEKS)
     bucky_npi=get_bucky(country_iso3,admin_level='adm0',min_date=LAST_TWO_MONTHS,max_date=FOUR_WEEKS,npi_filter='npi')
     bucky_no_npi=get_bucky(country_iso3,admin_level='adm0',min_date=LAST_TWO_MONTHS,max_date=FOUR_WEEKS,npi_filter='no_npi')
     
@@ -321,7 +195,6 @@ def draw_data_model_comparison(country_iso3,subnational_covid,who_covid,bucky_np
     else:
         print(f'metric {metric} not implemented')
         return False
-
     fig,axis=create_new_subplot(fig_title)
 
     # draw subnational reported cumulative cases
@@ -351,22 +224,6 @@ def draw_data_model_comparison(country_iso3,subnational_covid,who_covid,bucky_np
                           )
     plt.legend()
     fig.savefig(f'Outputs/{country_iso3}/current_{metric}.png')
-
-def create_new_subplot(fig_title):
-    fig,axis=plt.subplots(figsize=(FIG_SIZE[0],FIG_SIZE[1]))
-    
-    locator = mdates.AutoDateLocator(minticks=3, maxticks=7)
-    formatter = mdates.DateFormatter('%d %b')
-    axis.xaxis.set_major_locator(locator)
-    axis.xaxis.set_major_formatter(formatter)
-
-    axis.set_title(fig_title)
-    x_axis = axis.axes.get_xaxis()
-    x_label = x_axis.get_label()
-    x_label.set_visible(False)
-    axis.grid(linestyle='-', linewidth='0.5', color='black',alpha=0.2)
-    return fig,axis
-
 
 def create_subnational_map(country_iso3, parameters):
     # Total cases - four weeks projection
@@ -407,14 +264,6 @@ def calculate_subnational_trends(country_iso3, parameters):
     combined['cases_per_100k_change']=combined['cases_per_100k_change'].astype(int)
     # combined=combined[[parameters['adm1_name'],'cases_per_100k_change']]
     combined.to_csv(f'Outputs/{country_iso3}/ADM1_ranking.csv', index=False)
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("country_iso3", help="Country ISO3")
-    parser.add_argument('-d', '--download-covid', action='store_true',
-                        help='Download the COVID-19 data')
-    return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
+# global level functions
 import yaml
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import argparse
+import matplotlib.dates as mdates
+from scipy.optimize import curve_fit
+import numpy as np
+
+FIG_SIZE=(8,6)
+
+HLX_TAG_TOTAL_CASES = "#affected+infected+confirmed+total"
+HLX_TAG_TOTAL_DEATHS = "#affected+infected+dead+total"
+HLX_TAG_DATE = "#date"
+HLX_TAG_ADM2_PCODE='#adm2+pcode'
 
 def set_matlotlib(plt):
 
@@ -8,7 +23,128 @@ def set_matlotlib(plt):
     plt.rcParams['grid.linewidth'] = 0.5
     plt.rcParams.update({'font.size': 15})
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("country_iso3", help="Country ISO3")
+    parser.add_argument('-d', '--download-covid', action='store_true',
+                        help='Download the COVID-19 data')
+    return parser.parse_args()
+
 def parse_yaml(filename):
     with open(filename, "r") as stream:
         config = yaml.safe_load(stream)
     return config
+
+def download_url(url, save_path, chunk_size=128):
+    r = requests.get(url, stream=True)
+    with open(save_path, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            fd.write(chunk)
+    print(f'Downloaded "{url}" to "{save_path}"')
+
+def download_who_covid_data(url, save_path):
+    # download covid data from HDX
+    print(f'Getting upadated COVID data from WHO')
+    try:
+        download_url(url, save_path)
+    except Exception:
+        print(f'Cannot download COVID file from from HDX')
+
+def get_bucky(country_iso3,admin_level,min_date,max_date,npi_filter):
+    bucky_df=pd.read_csv(f'Bucky_results/{country_iso3}_{npi_filter}/{admin_level}_quantiles.csv')
+    bucky_df['date']=pd.to_datetime(bucky_df['date']).dt.date
+    bucky_df=bucky_df[(bucky_df['date']>=min_date) &\
+                        (bucky_df['date']<=max_date)]
+    bucky_df=bucky_df.set_index('date')
+    return bucky_df
+    
+def get_who(filename,country_iso3,min_date,max_date):
+    # Get national level data from WHO
+    who_covid=pd.read_csv(filename)
+    who_covid=who_covid[who_covid['ISO_3_CODE']==country_iso3]
+    who_covid['date_epicrv']=pd.to_datetime(who_covid['date_epicrv']).dt.date
+    who_covid=who_covid[(who_covid['date_epicrv']>=min_date) &\
+                        (who_covid['date_epicrv']<=max_date)]
+    who_covid=who_covid.set_index('date_epicrv')
+    return who_covid
+
+
+def get_subnational_covid_data(parameters,aggregate,min_date,max_date):
+    # get subnational from COVID parameterization repo
+    subnational_covid=pd.read_csv(parameters['subnational_cases_url'])
+    subnational_covid[HLX_TAG_DATE]=pd.to_datetime(subnational_covid[HLX_TAG_DATE]).dt.date
+    subnational_covid=subnational_covid[(subnational_covid[HLX_TAG_DATE]>=min_date) &\
+                                        (subnational_covid[HLX_TAG_DATE]<=max_date)]
+    if aggregate:
+        # date and adm2 are the unique keys
+        dates=sorted(set(subnational_covid[HLX_TAG_DATE]))
+        adm2pcodes=set(subnational_covid[HLX_TAG_ADM2_PCODE])
+        unique_keys=[HLX_TAG_DATE,HLX_TAG_ADM2_PCODE]
+        # create a multi-index to fill missing combinations with None values
+        mind = pd.MultiIndex.from_product([dates,adm2pcodes],names=unique_keys)
+        subnational_covid=subnational_covid.set_index(unique_keys).reindex(mind,fill_value=None)
+        # forward fill missing values for each pcode
+        subnational_covid=subnational_covid.groupby(HLX_TAG_ADM2_PCODE).ffill()
+        # sum by date
+        subnational_covid=subnational_covid.groupby(HLX_TAG_DATE).sum()
+    return subnational_covid
+
+
+def create_new_subplot(fig_title):
+    fig,axis=plt.subplots(figsize=(FIG_SIZE[0],FIG_SIZE[1]))
+    
+    locator = mdates.AutoDateLocator(minticks=3, maxticks=7)
+    formatter = mdates.DateFormatter('%d %b')
+    axis.xaxis.set_major_locator(locator)
+    axis.xaxis.set_major_formatter(formatter)
+
+    axis.set_title(fig_title)
+    x_axis = axis.axes.get_xaxis()
+    x_label = x_axis.get_label()
+    x_label.set_visible(False)
+    axis.grid(linestyle='-', linewidth='0.5', color='black',alpha=0.2)
+    return fig,axis
+
+def get_bucky_dt_reff(df_bucky):
+    # start fit
+    dates_proj = df_bucky.index
+    xfit=[(x-dates_proj[0]).days for x in dates_proj]
+    yfit = df_bucky['cumulative_cases_reported']
+    initial_caseload=yfit[0]
+    initial_parameters=[initial_caseload,0.03]
+    # # TODO check quality of the fit
+    popt, _ = curve_fit(func,xfit,yfit,p0=initial_parameters)
+    doubling_time_fit=np.log(2)/popt[1]
+    # parameters suggested by Matt
+    Tg = 7.
+    Ts = 5.
+    n = 3
+    f = .4
+    m = 2
+    r = np.log(2)/doubling_time_fit
+    Te=calc_Te(Tg, Ts, n, f)
+    reff=calc_Reff(m, n, Tg, Te, r)
+    # old method
+    # # https://www.acpjournals.org/doi/10.7326/M20-0504
+    # infectious_period=5.2
+    # # according to eq 1 in https://www.mdpi.com/2306-7381/7/1/2/htm#B17-vetsci-07-00002
+    # reff=1+(np.log(2)/doubling_time_fit)*infectious_period
+    return doubling_time_fit,reff
+
+def calc_Te(Tg, Ts, n, f):
+    num = 2.0 * n * f / (n + 1.0) * Tg - Ts
+    den = 2.0 * n * f / (n + 1.0) - 1.0
+    return num / den
+
+def calc_Reff(m, n, Tg, Te, r):
+    tdiff = Tg - Te
+    num = 2.0 * n * r / (n + 1.0) * (Tg - Te) * (1.0 + r * Te / m) ** m
+    den = 1.0 - (1.0 + 2.0 * r / (n + 1.0) * (Tg - Te)) ** (-n)
+    return num / den
+
+# not used at the moment
+# def calc_Ti(Te, Tg, n):
+    # return (Tg - Te) * 2.0 * n / (n + 1.0)
+
+def func(x, p0, beta):
+    return p0 * np.exp(x*beta)
