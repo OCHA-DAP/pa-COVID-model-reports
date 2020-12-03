cee6d043ee6e85603918e55ac4d0dfbf7d4dfd1e
Leonardo Milano <milanoleonardo@yahoo.it>
2020-12-03 14:29:26 +0100

    plots for SSD

--- historical_validation/historical_validation_ocha_bucky.py
+++ historical_validation/historical_validation_ocha_bucky.py
@@ -6,15 +6,20 @@ from datetime import datetime
 from utils import *
 import pandas as pd
 
-country_iso3='SSD'
-country_iso2='SS'
+country_iso3='AFG'
+country_iso2='AF'
 github_repo='https://raw.githubusercontent.com/OCHA-DAP/pa-COVID-model-reports'
 WHO_COVID_FILENAME='WHO_data/WHO-COVID-19-global-data.csv'
 WHO_DATA_COLOR='dodgerblue'
-download_csv=False
+NPI_COLOR='green'
+#these are the quantile values to consider for min and max projected numbers
+MIN_QUANTILE=0.25
+MAX_QUANTILE=0.75
+
+download_csv=True
 
 TODAY = datetime.today().date()
-EARLIEST_DATE = datetime.strptime('2020-02-24', '%Y-%m-%d').date()
+EARLIEST_DATE = datetime.strptime('2020-07-01', '%Y-%m-%d').date()
 
 
 def download_bucky_results(dir_path,country_iso3,github_repo):
@@ -43,49 +48,91 @@ def create_new_subplot(fig_title):
     axis.grid(linestyle='-', linewidth='0.5', color='black',alpha=0.2)
     return fig,axis
 
-def draw_data_model_comparison_new(country_iso3,who_covid,metric):
+def get_historical_bucky_collection(country_iso3,bucky_var):
+
+    data_folder=f'{dir_path}/historical_validation/data/{country_iso3}'
+    bucky_collection={}
+    for filename in os.listdir(data_folder):
+        if filename.endswith(".csv"):
+            filename=os.path.join(data_folder, filename)
+            df=pd.read_csv(filename)
+            bucky_metric=''
+            quantile='quantile'
+            if not quantile in df.columns:
+                quantile='q'
+            for var in bucky_var:
+                if var in df.columns:
+                    bucky_metric=var
+                    break
+            print(df.columns)
+            df=df.set_index('date')
+            out_df=pd.DataFrame({
+                'med':df[df[quantile]==0.5][bucky_metric],
+                'min':df[df[quantile]==MIN_QUANTILE][bucky_metric],
+                'max':df[df[quantile]==MAX_QUANTILE][bucky_metric],
+            })
+            out_df.index = pd.to_datetime(out_df.index)
+            bucky_collection[filename]=out_df
+        else:
+            continue
+    return bucky_collection
+
+
+def draw_data_model_comparison_new(country_iso3,metric):
     # plot the 4 inputs and save figure
-    if metric=='daily_reported_cases':
+    if metric=='cumulative_reported_cases':
+        who_var='Cumulative_cases'
+        bucky_var=['cumulative_reported_cases','cumulative_cases_reported']
+        fig_title='Daily reported cases'
+    elif metric=='daily_reported_cases':
         who_var='New_cases'
-        bucky_var='daily_reported_cases'
+        bucky_var=['daily_reported_cases','daily_cases_reported']
         fig_title='Daily reported cases'
     elif metric=='daily_deaths':
         who_var='New_deaths'
-        bucky_var='daily_deaths'
+        bucky_var=['daily_deaths']
         fig_title='Daily reported deaths'
     else:
         print(f'metric {metric} not implemented')
         return False
+
+    who_covid=get_who(WHO_COVID_FILENAME,country_iso2,min_date=EARLIEST_DATE,max_date=TODAY)
+    bucky_npi_collection=get_historical_bucky_collection(country_iso3,bucky_var)
+
     fig,axis=create_new_subplot(fig_title)
     # draw reported data by who
-    axis.bar(who_covid.index, who_covid[who_var],alpha=0.8,color=WHO_DATA_COLOR,label='WHO')
+    # axis.bar(who_covid.index, who_covid[who_var],alpha=0.8,color=WHO_DATA_COLOR,label='WHO')
     # compute rolling 7-day average
-    who_covid_rolling = who_covid[who_var].rolling(window=7).mean()
-    axis.plot(who_covid_rolling.index, who_covid_rolling,
-        lw=3,color=lighten_color(WHO_DATA_COLOR,1.6),label='WHO - 7d rolling average')
+    # who_covid_rolling = who_covid[who_var].rolling(window=7).mean()
+    # axis.plot(who_covid_rolling.index, who_covid_rolling,
+        # lw=3,color=lighten_color(WHO_DATA_COLOR,1.6),label='WHO - 7d rolling average')
+    
+        # draw WHO national reported numbers
+    axis.scatter(who_covid.index, who_covid[who_var],
+                     alpha=0.8, s=20,c=WHO_DATA_COLOR,marker='*',label='WHO')
 
-def get_historical_bucky(country_iso3):
-    data_folder=f'{dir_path}/historical_validation/data/{country_iso3}'
-    for filename in os.listdir(data_folder):
-        if filename.endswith(".csv"):
-            filename=os.path.join(data_folder, filename)
-            print(filename)
-            df=pd.read_csv(filename)
-            print(df)
-        else:
-            continue
+    for _, bucky_npi in bucky_npi_collection.items():
+        print(bucky_npi)
+        bucky_npi=bucky_npi[bucky_npi['med']>0]
+        bucky_npi['med'].plot(c=NPI_COLOR,ax=axis,label='Current NPIs maintained')
+    # axis.fill_between(bucky_npi_median.index,
+                        #   bucky_npi[bucky_npi['quantile']==MIN_QUANTILE][bucky_var],
+                        #   bucky_npi[bucky_npi['quantile']==MAX_QUANTILE][bucky_var],
+                        #   color=NPI_COLOR,alpha=0.2 
+
 
 if __name__ == "__main__":
 
     if download_csv:
         download_bucky_results(dir_path,country_iso3,github_repo)
     
-    # who_covid=get_who(WHO_COVID_FILENAME,country_iso2,min_date=EARLIEST_DATE,max_date=TODAY)
-    bucky_npi=get_historical_bucky(country_iso3)
+  
 
-    # draw_data_model_comparison_new(country_iso3,who_covid,'daily_reported_cases')
+    # draw_data_model_comparison_new(country_iso3,'daily_reported_cases')
+    draw_data_model_comparison_new(country_iso3,'cumulative_reported_cases')
+    # draw_data_model_comparison_new(country_iso3,'daily_deaths')
     # plt.legend()
-    # plt.show()
+    plt.show()
 
 
     
